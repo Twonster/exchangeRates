aa6255fcff1b581bb05349ed1fbc5cc4f76f37e0
Anton <leybov.anton@gmail.com>
2021-03-26 14:28:19 +0200

    add bot command /curse

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fetch = require('node-fetch');
 const TOKEN = '1639205327:AAGKLG9CX8ywSCC_jnXd-oAuYHzPBRpm5dg';
 const API_QUERY = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11'
 
+const bot = new Telegraf(TOKEN);
+
 const getRates = async function () {
     try {
         const response = await fetch(API_QUERY);
@@ -14,18 +16,15 @@ const getRates = async function () {
     }
 }
 
-
-const bot = new Telegraf(TOKEN)
-
-bot.start(async (ctx) => {
+bot.command('curse', async (ctx) => {
     const resp = await getRates();
     const ready = resp.reduce((acc, { ccy, base_ccy, buy, sale }) => {
-        return acc += `${ccy}:\nПокупка: ${buy}\nПродажа: ${sale} ${base_ccy}\n\n`
+        return acc += `${ccy}:\n⬇️ Покупка: ${Number(buy)} ${base_ccy}\n⬆️ Продажа: ${Number(sale).toFixed(2)} ${base_ccy}\n\n`
     },'')
     await ctx.reply(ready);
 })
 
 bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hi, how are you?'))
 bot.launch();
